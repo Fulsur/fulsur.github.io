2857751d1f26da8f341f98134aa972a641367f10
Flower Dreamer <fu2572809217@163.com>
2026-05-02 09:33:55 +0800

    init

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,25 @@
+import { defineUserConfig } from "vuepress";
+
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Blog Demo",
+      description: "A blog demo for vuepress-theme-hope",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "Fulsur",
+      description: "Fulsur的博客",
+    },
+  },
+
+  theme,
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
---
+++ src/zh/README.md
@@ -0,0 +1,17 @@
+---
+home: true
+layout: Blog
+icon: house
+title: 主页
+heroImage: https://theme-hope-assets.vuejs.press/logo.svg
+heroText: Fulsur的Blog
+heroFullScreen: true
+tagline: 双鸟暂时离分，必有重逢之日
+projects:
+  - icon: book
+    name: 书籍名称
+    desc: 书籍详细描述
+    link: https://你的书籍链接
+footer: 将年少一念倾余生信奉
+---
+
